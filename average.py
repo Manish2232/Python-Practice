77d40a2b1c48a63b5b40a20965ad14c63241bfb2
Manish Kumar <manishbgp555@gmail.com>
2026-06-10 17:36:44 +0530

    Added average program

---
+++ average.py
@@ -0,0 +1,7 @@
+# WAP to input 2 floating point numbers & print their average.
+
+num1 = float(input("Enter the 1st number : "))
+num2 = float(input("Enter the 2nd number : "))
+
+print("Average of the two number is : ", (num1+num2)/2)
+
